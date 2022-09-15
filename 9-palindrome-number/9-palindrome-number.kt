4ab573199bfeb2dc59a4f7039233cfa5e0191d99
HanDongWook <70149255+HanDongWook@users.noreply.github.com>
2022-09-15 20:29:21 +0900

    Time: 525 ms (15.27%), Space: 44 MB (8.89%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.kt
+++ 9-palindrome-number/9-palindrome-number.kt
@@ -3,7 +3,7 @@ import kotlin.math.abs
 class Solution {
     fun isPalindrome(x: Int): Boolean {
         if (x < 0) return false
-        val a = abs(x).toString().map { it.toString().toInt() }
+        val a = x.toString().map { it.toString().toInt() }
         for (i in a.indices) if (a[i] != a[a.lastIndex - i]) return false
         return true
     }
