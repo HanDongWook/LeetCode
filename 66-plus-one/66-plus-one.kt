97b344f71062694b872ef96a341fc131e6d8c47a
HanDongWook <jja9312345@gmail.com>
2022-10-01 14:04:06 +0900

    Time: 335 ms (16.51%), Space: 38.6 MB (7.77%) - LeetHub

---
+++ 66-plus-one/66-plus-one.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        val arr = IntArray(digits.size + 1)
+        for (i in 1..arr.lastIndex) arr[i] = digits[i-1]
+        arr[arr.lastIndex] = arr[arr.lastIndex] + 1
+        println(arr[arr.lastIndex])
+        for (i in arr.lastIndex downTo 1) {
+            if (arr[i] == 10) {
+                arr[i] = 0
+                arr[i - 1] += 1
+            } else break
+        }
+        return if (arr[0] == 0) {
+            arr.filterIndexed { index, _ -> index > 0 }.toIntArray()
+        } else arr
+    }
+}
